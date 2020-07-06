0b40b868b435c89ef40888ebd1187c2e482c443c
Niklas Preschern <niklas.preschern@gmx.at>
2020-07-06 22:37:44 +0200

    added lorenz96 dataset generator, causal ts via lagged VAR without dependency anomaly to data generator

--- testing_pipeline/data_generator.py
+++ testing_pipeline/data_generator.py
@@ -79,7 +79,7 @@ class dataset():
     # dependencies1 determine 0:n1 values
     # dependencies2 determine n1:n1+n2 values
     ###
-    def gen_data(self):
+    def gen_dep_anom_data(self):
         full_len = self.n1+self.n2
         data = np.zeros([self.features, full_len])
         # row indcies of caused ts from dependency structure 1 and 2
@@ -139,13 +139,90 @@ class dataset():
                 
         self.data = pd.DataFrame(data=data.T,index=range(full_len),columns=range(self.features))
                 
-
-
     def plot_data(self):
         pass
 
+    def make_var_stationary(self, beta, radius=0.97):
+        '''Rescale coefficients of VAR model to make stable.'''
+        p = beta.shape[0]
+        lag = beta.shape[1] // p
+        bottom = np.hstack((np.eye(p * (lag - 1)), np.zeros((p * (lag - 1), p))))
+        beta_tilde = np.vstack((beta, bottom))
+        eigvals = np.linalg.eigvals(beta_tilde)
+        max_eig = max(np.abs(eigvals))
+        nonstationary = max_eig > radius
+        if nonstationary:
+            return self.make_var_stationary(0.95 * beta, radius)
+        else:
+            return beta
 
+    def simulate_var(self, p, T, lag, sparsity=0.3, beta_value=1.0, sd=0.1, seed=0):
+        p = self.features
+        lag = self.lag
+        T = self.n1 + self.n2
+        sparsity = self.dep_dens
 
+        if seed is not None:
+            np.random.seed(seed)
+
+        # Set up coefficients and Granger causality ground truth.
+        GC = np.eye(p, dtype=int)
+        beta = np.eye(p) * beta_value
+
+        num_nonzero = int(p * sparsity) - 1
+        for i in range(p):
+            choice = np.random.choice(p - 1, size=num_nonzero, replace=False)
+            choice[choice >= i] += 1
+            beta[i, choice] = beta_value
+            GC[i, choice] = 1
+
+        beta = np.hstack([beta for _ in range(lag)])
+        beta = self.make_var_stationary(beta)
+
+        # Generate data.
+        burn_in = 100
+        errors = np.random.normal(scale=sd, size=(p, T + burn_in))
+        X = np.zeros((p, T + burn_in))
+        X[:, :lag] = errors[:, :lag]
+        for t in range(lag, T + burn_in):
+            X[:, t] = np.dot(beta, X[:, (t-lag):t].flatten(order='F'))
+            X[:, t] += + errors[:, t-1]
+
+        return X.T[burn_in:], beta, GC
         
+
+    def lorenz(self, x, t, F):
+    '''Partial derivatives for Lorenz-96 ODE.'''
+        p = len(x)
+        dxdt = np.zeros(p)
+        for i in range(p):
+            dxdt[i] = (x[(i+1) % p] - x[(i-2) % p]) * x[(i-1) % p] - x[i] + F
+
+        return dxdt
+
+
+    def simulate_lorenz_96(self, p, T, F=10.0, delta_t=0.1, sd=0.1, burn_in=1000,
+                        seed=0):
+        if seed is not None:
+            np.random.seed(seed)
+
+        p = self.features
+        T = self.n1 + self.n2
+
+        # Use scipy to solve ODE.
+        x0 = np.random.normal(scale=0.01, size=p)
+        t = np.linspace(0, (T + burn_in) * delta_t, T + burn_in)
+        X = odeint(self.lorenz, x0, t, args=(F,))
+        X += np.random.normal(scale=sd, size=(T + burn_in, p))
+
+        # Set up Granger causality ground truth.
+        GC = np.zeros((p, p), dtype=int)
+        for i in range(p):
+            GC[i, i] = 1
+            GC[i, (i + 1) % p] = 1
+            GC[i, (i - 1) % p] = 1
+            GC[i, (i - 2) % p] = 1
+
+        return X[burn_in:], GC
 
 
---
+++ testing_pipeline/test_algs/Neural-GC-master/test_file.py
@@ -0,0 +1,6 @@
+import synthetic
+
+p = 10
+T = 1000
+var_lag = 3
+X_np, beta, GC = synthetic.simulate_var(p, T, var_lag)
