65680ac599fb793ef1ca94e5ac7a0ec7c4054e0d
Niklas Preschern <niklas.preschern@gmx.at>
2020-07-29 16:48:48 +0200

    added argparse fns

--- testing_pipeline/gc_testing/tests/alg_test.py
+++ testing_pipeline/gc_testing/tests/alg_test.py
@@ -5,6 +5,7 @@ from apps.algorithms import Algorithm
 from apps.algorithm_loader import Algorithm_Loader
 import apps.data_generator as dg
 from test_algs.gc_ame.gc_ame_alg import GC_AME
+from test_algs.dca_bi_final.bi_cgl import BI_CGL
 
 dataset1 = dg.dataset()
 dataset1.gen_var_data()
@@ -13,6 +14,10 @@ args = {'data':dataset1.data, 'result_path':'result1', 'model_path':'result2'}
 alg_load = Algorithm_Loader(args)
 print(alg_load)
 
-args_alg = {"train_epochs": 100, "learning_rate": 0.01, "batch_size": 32, "granger_loss_weight": 0.05, "l2_weight": 12, "num_units": 23, "num_layers": 34}
-gc = GC_AME(args_alg)
-print(gc)
+args_ame = {"train_epochs": 100, "learning_rate": 0.01, "batch_size": 32, "granger_loss_weight": 0.05, "l2_weight": 12, "num_units": 23, "num_layers": 34}
+gc1 = GC_AME(args_ame)
+print(gc1)
+
+args_bicgl = {"train_epochs": 100, "learning_rate": 0.01, "batch_size": 32, "cuda":False}
+gc2 = BI_CGL(args_bicgl)
+print(gc2)
